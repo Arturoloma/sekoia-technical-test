e6f148f7550afb763ca9373b82d48091e155d856
arturoloma <arturoloma3d@gmail.com>
2025-11-09 22:41:13 +0100

    feat: added JokeAPI model

---
+++ src/app/models/joke-api.model.ts
@@ -0,0 +1,75 @@
+export enum JokeCategory {
+  ANY = 'Any',
+  CHRISTMAS = 'Christmas',
+  DARK = 'Dark',
+  MISC = 'Misc',
+  PROGRAMING = 'Programming',
+  PUN = 'Pun',
+  SPOOKY = 'Spooky',
+}
+
+export enum JokeFlag {
+  EXPLICIT = 'explicit',
+  NSFW = 'nsfw',
+  POLITICAL = 'political',
+  RACIST = 'racist',
+  RELIGIOUS = 'religious',
+  SEXIST = 'sexist',
+}
+
+export type JokeFlags = Record<JokeFlag, boolean>;
+
+export enum JokeType {
+  ANY = 'any',
+  SINGLE = 'single',
+  TWO_PART = 'twopart',
+}
+
+export enum JokeLanguage {
+  CZECH = 'cs',
+  ENGLISH = 'en',
+  FRENCH = 'fr',
+  GERMAN = 'de',
+  PORTUGUESE = 'pt',
+  SPANISH = 'es',
+}
+
+export interface JokeBaseResponse {
+  error: boolean;
+  timestamp?: number;
+}
+
+export interface JokeErrorResponse extends JokeBaseResponse {
+  additionalInfo: string;
+  causedBy: string[];
+  code: number;
+  error: true; // Narrow down for discriminated union
+  internalError: boolean;
+  message: string;
+}
+
+export interface Joke {
+  category: JokeCategory;
+  delivery?: string; // Second part of twopart jokes
+  flags: JokeFlags;
+  id: number;
+  joke?: string;
+  lang: JokeLanguage;
+  safe: boolean;
+  setup?: string; // First part of twopart jokes
+  type: JokeType;
+}
+
+// Success response type when it includes multiple jokes
+export interface MultipleJokesResponse extends JokeBaseResponse {
+  amount: number;
+  error: false; // Narrow down for discriminated union
+  jokes: Joke[];
+}
+
+// Success response type when it includes just one joke
+export interface SingleJokeResponse extends JokeBaseResponse, Joke {
+  error: false; // Narrow down for discriminated union
+}
+
+export type JokeApiResponse = JokeErrorResponse | MultipleJokesResponse | SingleJokeResponse;
